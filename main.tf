b799b816fe62f6c2bd534e5cb2be6b923b41ad88
firoz03 <firoj.siddique@unthinkable.co>
2021-09-01 16:19:47 +0530

    main.tf update

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
-provider "aws" {
-  region = var.region
-  //profile = var.profile
+module "iam_group_with_policies" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+  version = "~> 3.0"
+
+  name = var.groupname
+
+  group_users = var.groupusername
+
+  attach_iam_self_management_policy = true
+
+ custom_group_policy_arns = var.custom_group_policy_arns
+
+  custom_group_policies = [
+    {
+      name   = "AllowS3Listing"
+      policy = data.aws_iam_policy_document.sample.json
+    }
+  ]
 }
 
-module "iam_user" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
-  version = "~> 3.0"
-  pgp_key = "keybase:test"
-  name          = var.name
-  force_destroy = true
-  password_reset_required = false
-}
