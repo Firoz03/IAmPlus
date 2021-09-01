abb86a708116af03dc68b8c0bf83023799367d8e
Firoz03 <86497771+Firoz03@users.noreply.github.com>
2021-09-01 16:08:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 module "iam_user" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
   version = "~> 3.0"
-
+  pgp_key = "keybase:test"
   name          = var.name
   force_destroy = true
   password_reset_required = false
