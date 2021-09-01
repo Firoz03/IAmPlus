e1cbb43b90eef7e8899e43c0d32d373e2b5207b1
firoz03 <firoj.siddique@unthinkable.co>
2021-09-01 16:22:47 +0530

    main update

---
+++ variable.tf
@@ -0,0 +1,42 @@
+variable "create_group" {
+  description = "Whether to create IAM group"
+  type        = bool
+  default     = true
+}
+
+variable "groupname" {
+  description = "Name of IAM group"
+  type        = string
+}
+
+variable "groupusername" {
+  description = "name of the users"
+  type        = list(string)
+  default = [
+      "user",
+      "Ruhee"
+
+    ]
+  
+}
+variable "custom_group_policy_arns" {
+  description = "List of IAM policies ARNs to attach to IAM group"
+  type        = list(string)
+  default     = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
+
+variable "custom_group_policies" {
+  description = "List of maps of inline IAM policies to attach to IAM group. Should have `name` and `policy` keys in each element."
+  type        = list(map(string))
+  default     = []
+}
+
+data "aws_iam_policy_document" "sample" {
+  statement {
+    actions = [
+      "s3:ListBuckets",
+    ]
+
+    resources = ["*"]
+  }
+}
